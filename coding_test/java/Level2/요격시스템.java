60affe9f2d37082dbc15b432ce514426d385d5ea
no-cy <63999547+no-cy@users.noreply.github.com>
2023-12-07 20:54:53 +0900

    요격시스템.java 완료 ~

--- coding_test/java/Level2/요격시스템.java
+++ coding_test/java/Level2/요격시스템.java
@@ -1,8 +1,27 @@
-package Level2;
+import java.util.Arrays;
+import java.util.Comparator;
 
 class Solution {
     public int solution(int[][] targets) {
         int answer = 0;
+        
+        Arrays.sort(targets, Comparator.comparingInt(o -> o[1]));     // 미사일 종료 지점을 기준으로 정렬
+
+        int interceptCount = 0;
+        int lastInterception = -1;
+
+        for (int[] target : targets) {
+            int start = target[0];
+            int end = target[1];
+
+            if (start + 0.1 > lastInterception) {                      // 한 개의 폭격 미사일 끝 지점보다 시작지점이 더 높은 좌표의 미사일이 있을 경우
+                interceptCount++;                                      // 요격 미사일 추가
+                lastInterception = end;                                // 끝 지점 업데이트
+            }
+        }
+
+        answer = interceptCount;
+
         return answer;
     }
 }
